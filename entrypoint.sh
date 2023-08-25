2a44eb86489f120944ad7ae740ef23c0bc0b179b
Spinka8khy <137860310+Spinka8khy@users.noreply.github.com>
2023-08-25 10:43:25 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量，WS 路径前缀。(注意:伪装路径不需要 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
 WSPATH=${WSPATH:-'ecdeb434'}
-UUID=${UUID:-'bff506b0-0037-46b5-9b95-b31b8b3cc907'}
+UUID=${UUID:-'f1ecfdbc-d243-4601-8850-b8c6bef5ed48'}
 
 # 安装系统依赖
 check_dependencies() {
